9db329860c0e7ad57ed34ca40f4cea28a3f97cf8
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-14 16:24:36 +0630

    Add project files.

---
+++ DotNet8WebApi.MysqlSample/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8WebApi.MysqlSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+    }
+}
