7ea719e9311a9ff85a902923885b790f4c800d24
Linn Thit Htoo <lth1212001@gmail.com>
2024-09-14 20:05:12 +0630

    [ Added ] GetBlogs Endpoint

--- DotNet8WebApi.MysqlSample/Controllers/BlogController.cs
+++ DotNet8WebApi.MysqlSample/Controllers/BlogController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DotNet8WebApi.MysqlSample.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
 
 namespace DotNet8WebApi.MysqlSample.Controllers
 {
@@ -7,5 +9,52 @@ namespace DotNet8WebApi.MysqlSample.Controllers
     [ApiController]
     public class BlogController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public BlogController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBlogs()
+        {
+            try
+            {
+                var lst = new List<BlogModel>();
+
+                var connection = new MySqlConnection(_configuration.GetConnectionString("DbConnection"));
+                await connection.OpenAsync();
+
+                string query = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent FROM Tbl_Blog";
+                var command = new MySqlCommand(query, connection);
+                MySqlDataAdapter adapter = new();
+                MySqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    int id = reader.GetInt32("BlogId");
+                    string blogTitle = reader.GetString("BlogTitle");
+                    string blogAuthor = reader.GetString("BlogAuthor");
+                    string blogContent = reader.GetString("BlogContent");
+
+                    lst.Add(new BlogModel()
+                    {
+                        BlogId = id,
+                        BlogTitle = blogTitle,
+                        BlogAuthor = blogAuthor,
+                        BlogContent = blogContent
+                    });
+                }
+
+                await connection.CloseAsync();
+
+                return Ok(lst);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
---
+++ DotNet8WebApi.MysqlSample/Services/AdoDotNetService.cs
@@ -0,0 +1,16 @@
+﻿using MySqlConnector;
+
+namespace DotNet8WebApi.MysqlSample.Services
+{
+    public class AdoDotNetService
+    {
+        private readonly IConfiguration _configuration;
+
+        public AdoDotNetService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private MySqlConnection GetMySqlConnection() => new(_configuration.GetSection("DbConnection").Value!);
+    }
+}
